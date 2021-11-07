e23c59a5aeeb6a798e9e6d24f3387d3fad364a7b
Sean Delaney <seandelaney42@gmail.com>
2021-11-07 14:28:09 -0500

    Updated Companies Controller Error
    One of the previous edits messed up the companies controller export so I had to fix that real quick.

--- server/controllers/companiesController.js
+++ server/controllers/companiesController.js
@@ -15,14 +15,8 @@ VALUES
 const companyDelete = `DELETE FROM companies
 WHERE companies.company_id = :company_id;`;
 export default {
-<<<<<<<< HEAD:server/controllers/companiesController.js
   companyGet,
   companyPut,
   companyPost,
   companyDelete
 };
-========
-  companyGet, companyPut, companyPost, companyDelete
-};
-
->>>>>>>> 0f0ad07d7df8d89c7f23ae6161d532da85277934:routes/controllers/companiesController.js
