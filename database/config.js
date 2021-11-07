79089d4ac255171cd45c5df2fd11f57bf9c9443d
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2021-11-06 20:28:34 -0400

    Update-config.js
    As of November 6th

--- database/config.js
+++ database/config.js
@@ -1,23 +1,10 @@
 export default {
   development: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
-    dialect: 'mysql'
-  },
-  test: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
-    dialect: 'mysql'
-  },
-  production: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
-    dialect: 'mysql'
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'INST377-Group-14-FinalProject',
+    host: '18.212.135.23',
+    dialect: 'mysql' 
   }
+  
 };
