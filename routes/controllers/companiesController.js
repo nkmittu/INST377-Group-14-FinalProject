cdae0b9e4616385d0e75a44b400b7a7a4647b2ee
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2021-11-06 20:18:06 -0400

    lab_10_pt2

--- routes/controllers/companiesController.js
+++ routes/controllers/companiesController.js
@@ -18,19 +18,3 @@ export default {
   companyGet, companyPut, companyPost, companyDelete
 };
 
-const artistsGet = `SELECT a.artist_id,
-  a.birth_name
-FROM
-  artists a ; `;
-const artistsPut = `UPDATE artists
-SET artist_name=:artist_name
-WHERE artist_id=:artist_id;`;
-const artistsPost = `INSERT INTO artists
-(artist_id)
-VALUES
-(:artist_id);`;
-const artistsDelete= `DELETE FROM artists
-WHERE artists.artist_id = :artist_id;`;
-export default {
-  artistsGet, artistsPut, artistsPost, artistsDelete
-};
