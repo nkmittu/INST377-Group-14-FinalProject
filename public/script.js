338ba256ba1137325ed5b341f465856e42c51ddb
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2021-11-16 20:42:22 -0500

    celebs_script.js_unfinished

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+//Sean
 /* eslint-disable arrow-body-style */
 /* eslint-disable linebreak-style */
 async function companyActions() {
@@ -36,3 +37,43 @@ async function companyActions() {
     console.log('listening'); });
 }
 window.onload = companyActions;
+
+//Elvis
+async function artistsActions() {
+  const searchInput = document.querySelector('.search');
+  const endpoint = '/api/artists';
+  const request = await fetch(endpoint);
+  const cities = await request.json();
+  const suggestions = document.querySelector('.suggestions');
+  console.log('artists')
+
+  function findMatches(wordToMatch, artist) {
+    return cities.filter((place) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      
+      return place.stage_name.match(regex) || place.company_name.match(regex) || place.birth_name.match(regex);
+    });
+  }
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, artist);
+    console.log(matchArray);
+    const html = matchArray.map((place) => {
+      return `
+        <li>
+          <span class = 'name'>${place.birth_name} also known as ${place.stage_name} works with ${place.company_name}</span>
+        </li>
+      `;
+    }).join('');
+    console.log(html);
+    suggestions.innerHTML = html;
+    if (!event.target.value) {
+      suggestions.innerHTML = '';
+    }
+  }
+  searchInput.addEventListener('keyup', (evt) => { 
+    displayMatches(evt);
+    console.log('listening'); });
+}
+window.onload = artistsActions;
+
+
