6194aca099eabb44205d275eadc848606d5c865b
Thitna Gruga <thitnasisay24@gmail.com>
2021-11-30 17:42:57 -0500

    Create musical_awardsController.js

---
+++ server/controllers/musical_awardsController.js
@@ -0,0 +1,12 @@
+
+const musical_awardsGet = `SELECT a.artist_id',
+        a.birth_name, a.stage_name
+    FROM 
+        artists a `
+
+const musical_awardsPut = `UPDATE musical_award 
+    
+SET 
+
+`
+    
